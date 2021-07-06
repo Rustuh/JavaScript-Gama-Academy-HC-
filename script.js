1e7e13aaba8b400a0cca5b5f1b89e0c157f12712
Rustuh <lipefel@gmail.com>
2021-07-06 13:48:44 -0300

    Primeiro commit Variaveis

---
+++ script.js
@@ -0,0 +1,24 @@
+var myNickname = "Rustuh";
+
+let language = "Javascript";
+
+const pattern = "ECMAScript";
+
+console.log(myNickname)
+
+/*exemplo de como usar variaveis*/
+
+/* Pode ser usado (VAR são declarações de escpo global ou escopo de funções )
+{
+    var age = 32
+}
+
+console.log(age)
+*/
+
+/* Não pode ser usado (LET e CONST são de escopo bloqueado)
+{
+    let age = 32
+}
+    console.log(age)
+*/
