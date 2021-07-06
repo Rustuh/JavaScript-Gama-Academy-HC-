ca0880e89689d792b3f58bab5c2f093e478a1386
Rustuh <lipefel@gmail.com>
2021-07-06 14:06:03 -0300

    Aula 02 Tipo de dados

--- script.js
+++ script.js
@@ -22,3 +22,56 @@ console.log(age)
 }
     console.log(age)
 */
+
+//Tipo de dados
+/*
+var myName = "Felipe" //string
+
+var age = 33 //number
+
+var height = 1.70 //number
+
+null
+undefined
+
+var isStudent = false //bolean
+*/
+
+//Obejeto, array e function
+
+/*
+
+//Obejeto
+var person = {
+    "name": "Felipe",
+    "age": 33
+}
+
+//array
+
+var students = [ "Felipe", "Orlando", "Rustuh"]
+
+//function
+
+function sum(a,b){
+    return a + b
+}
+
+*/
+
+//operador typeof
+/*
+console.log(typeof 32) //number
+
+var isStudent = true;
+console.log(typeof isStudent) //bolean
+
+var object = null;
+console.log(typeof object) //objeto
+
+function sum(){
+    return 5 + 2
+}
+
+console.log(typeof sum)
+*/
