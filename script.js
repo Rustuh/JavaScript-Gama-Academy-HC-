7ca3f8f5c521590fea50449f9cf9c06eefbb8220
Rustuh <lipefel@gmail.com>
2021-07-06 22:52:38 -0300

    Aula 07 Classes

--- script.js
+++ script.js
@@ -230,3 +230,63 @@ decremento (--)
     console.log(sum(4,5));
 
 */
+
+/*
+
+    //Classes
+
+    //criando objeto
+
+    class Book {
+        constructor(title, author, pages){
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }
+
+        read() {
+            return `Estou lendo ${this.title}`
+        }
+    }
+
+        let book = new Book("Algoritimos para viver", "Brian", 500);
+        let otherBook = new Book("Um Exu em Nova York", "Cidinha da Silva", 100);
+
+        console.log(book, otherBook)
+
+    //Herança
+
+        class ITBook extends Book {
+            constructor(title, author, pages, technology){
+                super(title, author, pages);
+                this.technology = technology;
+
+            }
+        let itBook = new ITBook("Algoritimos para viver, "Brian", 500, "Algoritimos");
+
+        console.log(itBook.title);
+        
+    }
+
+
+    //Encapsulamento
+
+        class Person {
+            constructor(name) {
+                this._name = name;
+            }
+            
+            get name() {
+                return this._name;
+            }
+
+            set name(value){
+                this._name = value;
+            }
+        }
+
+        let person = new Person("Felipe");
+        person.name = "Rustuh"
+        console.log(person.name)
+
+*/
